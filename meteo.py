3372cae8358d24e0e121a5f4e8ffeee30513fe4c
PACI Marion (ENGIE Local Energy Infrastructures) <jm6690@engie.com>
2026-06-28 19:22:13 +0200

    Ajout fonction comptage jours chauds

--- meteo.py
+++ meteo.py
@@ -4,6 +4,7 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # car problème réseau
                                                                     # entreprise
 
+
 # ici l'API renvoit directement la température en Celsius, donc pas besoin de conversion
 def KelvinToCelsius(Tkelvin):
     """Convert temperature from Kelvin to Celsius
@@ -38,14 +39,33 @@ def GetTemperatureFromAPI(ville, api_key):
     return temperature
 
 
+def FonctionExercice7(temperatures_list):
+    
+    i = 0
+
+    for temperature in temperatures_list:
+        if temperature > 20:
+            i += 1
+    
+    return i
+
+
 if __name__ == "__main__":
     
+    # # Exercice 5
     # Tkelvin = 40+273.15
     # Tcelsius = KelvinToCelsius(Tkelvin)  
     # MessageVilleEtTemperature("Paris", Tcelsius)
 
-    api_key = "62b32cf2876cbabbd135b211f0b4c4f3"
-    ville = "Paris"
+    # # Exercice 6
+    # api_key = "62b32cf2876cbabbd135b211f0b4c4f3"
+    # ville = "Paris"
+    # temperature = GetTemperatureFromAPI(ville, api_key)
+    # MessageVilleEtTemperature(ville, temperature)
 
-    temperature = GetTemperatureFromAPI(ville, api_key)
-    MessageVilleEtTemperature(ville, temperature)
+    # Exercice 7
+    ListeTemp = [20, 18, 22, 25, 19, 21, 17, 23, 20]
+    NombreJoursDansListe = len(ListeTemp)
+    compteur = FonctionExercice7(ListeTemp)
+    print(f"Il y a dans cette liste {compteur} jour(s) sur {NombreJoursDansListe} avec "
+           "une température supérieure à 20°C")
